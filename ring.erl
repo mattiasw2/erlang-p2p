b6cc5dd152473ec62460c4e247925cf24caf74d9
jonathan.sarton <jonathan.sarton@gmail.com>
2013-04-18 12:33:19 +0000

    structure de l'anneau

---
+++ ring.erl
@@ -0,0 +1,56 @@
+-module(ring).
+-export([launch/3,init/3]).
+
+splice(X,Y) ->
+    X ! {next,self()},
+    receive NX ->
+        Y ! {next,self()},
+        receive NY ->
+            Y ! {set,NX,self()},
+            X ! {set,NY,self()},
+            receive ok -> ok end,
+            receive ok -> ok end
+        end
+    end.
+
+init(Mod,Fun,Param) ->
+    wait(Mod,Fun,Param,self()).
+
+wait(Mod,Fun,Uid,Next) ->
+    receive
+        {next,Who} -> Who ! Next, wait(Mod,Fun,Uid,Next) ;
+        {set,NNext,Who} -> Who ! ok, wait(Mod,Fun,Uid,NNext) ;
+        start -> Mod:Fun(Uid,Next)
+    end.
+
+create(Mod,Fun,{Cle,{Name,Node}},1) ->
+	Pid = spawn(?MODULE,init,[Mod,Fun,[{Name,Node},Cle,0]]),
+	register(Name,Pid),
+	{Pid,[Pid]};
+	
+create(Mod,Fun,[X|L],N) ->
+	{X,A} = create(Mod,Fun,L,N-1),
+	{Y,B} = create(Mod,Fun,X,1),
+	R = walk({X,A},N-2),
+	splice(R,Y),
+	{X, A ++ B}.
+	
+launch(Mod,Fun,List) ->
+	io:format("~w", List),
+	ListId = lists:map(fun(X) -> {crypto:sha(term_to_binary({X,node()})),
+									{X,node()}} end,List),			
+	ListSorted = lists:sort(ListId),
+	N = length(ListSorted),
+	{X,L} = create(Mod,Fun,ListSorted,N).
+    
+walk(X,0) -> X ;
+walk(X,N) ->
+    X ! {next,self()},
+    receive Z ->
+        walk(Z,N-1)
+    end.
+
+go([]) -> ok ;
+go([X|L]) -> X ! start, go(L).
+
+bidon([MyId,MyKey,Val],Next) -> ok.
