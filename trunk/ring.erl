08b0d8e64fea5b5e639040952a9cac867d4a38c5
jonathan.sarton@gmail.com <jonathan.sarton@gmail.com@244490e1-c105-eee1-e796-7181da39c609>
2013-04-23 13:28:30 +0000

    debug node et modif ring

--- trunk/ring.erl
+++ trunk/ring.erl
@@ -1,5 +1,5 @@
 -module(ring).
--export([launch/3,init/3]).
+-export([launch/3,init/3,create/4,go/1]).
 
 splice(X,Y) ->
     X ! {next,self()},
@@ -20,28 +20,30 @@ wait(Mod,Fun,Uid,Next) ->
     receive
         {next,Who} -> Who ! Next, wait(Mod,Fun,Uid,Next) ;
         {set,NNext,Who} -> Who ! ok, wait(Mod,Fun,Uid,NNext) ;
-        start -> Mod:Fun(Uid,Next)
+        start -> io:format("~w ~w ~w ~w", [Mod,Fun,Uid,Next]), Mod:Fun(Uid,Next)
     end.
 
-create(Mod,Fun,{Cle,{Name,Node}},1) ->
+create(Mod,Fun,[{Cle,{Name,Node}}],1) ->
 	Pid = spawn(?MODULE,init,[Mod,Fun,[{Name,Node},Cle,0]]),
+	%io:format("~w", [Pid]),
 	register(Name,Pid),
 	{Pid,[Pid]};
 	
 create(Mod,Fun,[X|L],N) ->
-	{X,A} = create(Mod,Fun,L,N-1),
-	{Y,B} = create(Mod,Fun,X,1),
-	R = walk({X,A},N-2),
-	splice(R,Y),
-	{X, A ++ B}.
+	{P,B} = create(Mod,Fun,L,N-1),
+	{Y,A} = create(Mod,Fun,[X],1),
+	%R = walk({X,A},N-2),
+	splice(P,Y),
+	{P, A ++ B}.
 	
 launch(Mod,Fun,List) ->
-	io:format("~w", List),
+	%io:format("~w", List),
 	ListId = lists:map(fun(X) -> {crypto:sha(term_to_binary({X,node()})),
 									{X,node()}} end,List),			
 	ListSorted = lists:sort(ListId),
 	N = length(ListSorted),
 	{X,L} = create(Mod,Fun,ListSorted,N).
+	%go(L).
     
 walk(X,0) -> X ;
 walk(X,N) ->
@@ -52,5 +54,3 @@ walk(X,N) ->
 
 go([]) -> ok ;
 go([X|L]) -> X ! start, go(L).
-
-bidon([MyId,MyKey,Val],Next) -> ok.
