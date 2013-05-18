0ae1edcf513e7abdee49819a869ddd4ac82559f9
tristan.monediere@gmail.com <tristan.monediere@gmail.com@244490e1-c105-eee1-e796-7181da39c609>
2013-05-18 22:51:24 +0000

    correction des messages de base lookup, get,put ne devrait pus boucler (a retester apres avoir dormi)

--- trunk/node.erl
+++ trunk/node.erl
@@ -5,42 +5,53 @@
 	%besoin d'implementer les finger tables
 	% <=> parcours de liste chainee circulaire
 
+%compare si une clef est entre moi et mon voisin
+between_me_next(Key,MyKey, MyNeighbor)->
+	NextKey = crypto:sha(term_to_binary(MyNeighbor)),
+	if 
+		Key >= MyKey andalso Key < NextKey -> 
+			true;
+		true ->
+			false
+	end.
+
+lookup(Key,Who,MyKey,Succ)->
+	case (between_me_next(Key,MyKey,Succ)) of
+				true  -> Who ! {res_lookup,Succ};
+				false -> Succ ! {lookup,Key,Who}
+	end.
+
+get(Key,Who,MyKey,Succ)->
+	case (between_me_next(Key,MyKey,Succ)) of
+				true  -> Succ ! {getVal,Who};
+				false -> Succ ! {get,Key,Who}
+	end.
+
+put(Key,Val,MyKey,Succ)->
+	case (between_me_next(Key,MyKey,Succ)) of
+				true  -> Succ ! {changeVal,Val};
+				false -> Succ ! {put,Key,Val}
+	end.
+
 wait(MyId,MyKey,MyVal,Pred,Succ)->
 	receive 
 		%primitives
 		{lookup,Key,Who} -> %changer par une fonction plus pratique
 	io:format("~w : receive ~w,~w,~w ~n",[MyId,lookup,Key,Who]),
-			if 
-				Key == MyKey ->
-					Who ! {res_lookup,self()};
-				Key /= MyKey ->
-					Succ ! {lookup,Key,self()},
-					receive
-						{res_lookup,Pid} -> 
-							Who ! {res_lookup,Pid}
-					end
-			end,
+			lookup(Key,Who,MyKey,Succ),
 			wait(MyId,MyKey,MyVal,Pred,Succ);
 
 		{get,Key,Who} ->
-			self() ! {lookup,Key,self()},
-			receive {res_lookup, Pid} -> 
-				Pid ! {get_val,self()},
-				receive 
-					{get_val_res,Val} -> 
-						Who ! {get_val_res,Val}
-				end
-			end,
+			get(Key,Who,MyKey,Succ),
 			wait(MyId,MyKey,MyVal,Pred,Succ);
 			
 		{put,Key,Val} ->
-			self() ! {lookup,Key,self()},
-			receive {res_lookup, Pid} -> 
-				Pid ! {change_val,Val}
-			end,
+			put(Key,Val,MyKey,Succ),
 			wait(MyId,MyKey,MyVal,Pred,Succ);
 		%retour des valeurs
-		{get_val,Who} -> Who ! {get_val_res,MyVal},
+		{get_val,Who} -> 
+			Who ! {get_val_res,MyVal},
 			wait(MyId,MyKey,MyVal,Pred,Succ);
-		{change_val,Val} -> wait(MyId,MyKey,Val,Pred,Succ)
+		{change_val,Val} -> 
+			wait(MyId,MyKey,Val,Pred,Succ)
 	end.
