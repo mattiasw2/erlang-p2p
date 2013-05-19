ce6a6e7194d94d2a64da91f5085ae50fcf940d22
nathmoller@gmail.com <nathmoller@gmail.com@244490e1-c105-eee1-e796-7181da39c609>
2013-05-19 18:31:01 +0000

    quelques debugs sur put, get et finger

--- trunk/launch.sh
+++ trunk/launch.sh
@@ -8,21 +8,28 @@ LL=lists:map(fun(X) ->
     list_to_atom(X) end,L),
 LL,
 {X,List} = ring:launch(node,wait,LL),
+
 %lancement 
 ring:go(List),
 N=length(List),
 test(List,N).
 
 test([X|L],N)->
-    %C=get(L,random:uniform(N-1)),
-    %io:format("~w ~n",[X]),
-    {e,trt@debug} ! {lookup,<<141,186,206,103,134,101,197,13,232,151,223,244,198,14,130,52,173,111,182,83>>,self()},
+    {e,trt@debug} ! {lookup,<<141,186,206,103,134,101,197,13,232,151,223,244,198,14,130,52,173,111,182,83>>,self()},    
     receive 
         {res_lookup,Node} -> 
-            io:format(" main receive ~w ~n",[Node])
-    end.
+            io:format("LOOKUP : main receive ~w ~n",[Node])
+    end,			 
+	{c,trt@debug} ! {put,<<141,186,206,103,134,101,197,13,232,151,223,244,198,14,130,52,173,111,182,83>>,777}, 
+	timer:sleep(500),   
+	{b,trt@debug} ! {get,<<141,186,206,103,134,101,197,13,232,151,223,244,198,14,130,52,173,111,182,83>>,self()}, 
+	timer:sleep(500),
+    receive
+    	{get_val_res,Val} ->
+        io:format("GET : main receive ~w ~n",[Val])
+    end,
+    timer:sleep(500),
+    c:flush().
 
-get([{A,B}],N) -> A;
-get([{A,B}|L],0) -> A;
-get([{A,B}|L],N) ->
-    get(L,N - 1).
+
+
