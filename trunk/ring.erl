d6b02421e30b24c96398dc0a08381f4bc5532a39
tristan.monediere@gmail.com <tristan.monediere@gmail.com@244490e1-c105-eee1-e796-7181da39c609>
2013-05-19 11:34:42 +0000

    correction d'un probleme lorsque la clef demander appartient au noeud 0

--- trunk/ring.erl
+++ trunk/ring.erl
@@ -2,12 +2,12 @@
 -export([launch/3,init/3,create/4,go/1]).
 
 splice(X,Y) ->
-    io:format("splice Y: ~w, X: ~w ~n",[Y,X]),
+    %io:format("splice Y: ~w, X: ~w ~n",[Y,X]),
     X ! {next,self()},
     receive NX ->
         Y ! {next,self()},
         receive NY ->
-            io:format("splice NY: ~w, NX:~w ~n",[NY,NX]),
+            %io:format("splice NY: ~w, NX:~w ~n",[NY,NX]),
             Y ! {set,NX,self()},
             X ! {set,NY,self()},
             receive ok -> ok end,
@@ -39,7 +39,7 @@ create(Mod,Fun,[{Cle,{Name,Node}}],1) ->
 create(Mod,Fun,[X|L],N) ->
 	{P,B} = create(Mod,Fun,L,N-1),
 	{Y,A} = create(Mod,Fun,[X],1),
-    aff([Y]),
+    %aff([Y]),
 	%R = walk({P,B},N-2),
 	splice(P,Y),
     %splice(P,R),
@@ -50,7 +50,7 @@ launch(Mod,Fun,List) ->
 	ListId = lists:map(fun(X) -> {crypto:sha(term_to_binary({X,node()})),
 									{X,node()}} end,List),			
 	ListSorted = lists:sort(ListId),
-    %aff(ListSorted),
+    aff(ListSorted),
 	N = length(ListSorted),
     %io:format("nbelem : ~w~n",[N]),
 	{A,L} = create(Mod,Fun,ListSorted,N).
