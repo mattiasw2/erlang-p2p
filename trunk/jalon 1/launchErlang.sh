f7ca719af90cddd756b2ca791ba1d8b5a27f76d2
nathmoller@gmail.com <nathmoller@gmail.com@244490e1-c105-eee1-e796-7181da39c609>
2013-05-20 21:38:53 +0000

    jalon 1 ok

--- trunk/jalon 1/launchErlang.sh
+++ trunk/jalon 1/launchErlang.sh
@@ -12,20 +12,39 @@ LL,
 %lancement 
 ring:go(List),
 N=length(List),
-createJInterface(List),
-test(List,N).
+test(List,N),
+createJInterface(List).
+
 
 test([X|L],N)->
-    			 	
-    %{papa,trt@debug} ! {calcFT, 20},
-    timer:sleep(4500).
+    			 	    
+%% LOOKUP   
+
+	{alpha, trt@debug} ! {lookup,<<37,223,227,122,143,5,161,114,235,127,71,120,216,88,158,38,130,0,217,156>>,self()},
+	receive
+    	{res_lookup,Succ} -> 
+			io: format(" Le résultat du lookup est : ~w ~n", [Succ])
+    end,
+   
+%% PUT
+	{papa, trt@debug} ! {put,<<37,223,227,122,143,5,161,114,235,127,71,120,216,88,158,38,130,0,217,157>>,777},
+	timer:sleep(500),
+
+
+%% GET
+	{papa, trt@debug} ! {get,<<37,223,227,122,143,5,161,114,235,127,71,120,216,88,158,38,130,0,217,157>>,self()},
+	receive
+		{get_val_res, Who} ->
+			io: format(" Le résultat du get est : ~w~n", [Who])		
+	end.
+
+  
    
 createJInterface(List) ->
-	io:format("Je suis dans createJInterface avec la liste : ~w ~n",[List]),
 	{j,'java@localhost'} ! {self(), List},
 	receive
 		{Res} -> 
-			io:format("~w ! Echange avec Java ok ! ~p", [Res])
+			io:format("~w ! Echange avec Java ok !", [Res])
 	end.
 
 
