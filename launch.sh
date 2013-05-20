b578c3308243440403ad534e64968fcf52a33569
nathmoller <nathmoller@gmail.com>
2013-05-20 18:53:24 +0000

    etat actuel

--- launch.sh
+++ launch.sh
@@ -16,8 +16,16 @@ test(List,N).
 
 test([X|L],N)->
     			 	
-    {obiwan,trt@debug} ! {calcFT, 20},
-    timer:sleep(4500).
+    {d,trt@debug} ! {calcFT, 160},
+    timer:sleep(4500),
+    {f,trt@debug} ! {logSearch,<<5,197,183,236,203,12,112,78,187,68,106,146,233,225,31,85,75,38,157,137>>},
+    timer:sleep(500),
+    {y,trt@debug} ! {debug_aff_ft,self()},
+    timer:sleep(500),
+    receive
+    	{R}->
+    	ring:aff(R)
+    end.
    
 
 
